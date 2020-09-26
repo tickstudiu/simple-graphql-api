89b86d29f2a706cac5e880491b2dcc09532b325b
tickstudiu <tickstudiu@gmail.com>
2020-09-26 18:18:09 +0800

    update mutation create user

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga'
+import { v4 as uuidv4 } from 'uuid'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
@@ -69,6 +70,10 @@ const typeDefs = `
         me: User!
         post: Post!
     }
+    
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int): User!
+    }
 
     type User {
         id: ID!
@@ -136,6 +141,24 @@ const resolvers = {
                 body: '',
                 published: false
             }
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info){
+            const emailTaken = users.some((user) => user.email === args.email)
+
+            if(emailTaken) throw new Error("Email taken.")
+
+            const user = {
+                id: uuidv4(),
+                name: args.name,
+                email: args.email,
+                age: args.age
+            }
+
+            users.push(user)
+
+            return user
         }
     },
     Post: {
