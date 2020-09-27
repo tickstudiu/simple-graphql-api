942687f750178952ce4ddbe6b405bef00945eb64
Wanchalerm S <tickstudiu@gmail.com>
2020-09-27 17:30:59 +0700

    update plugin

--- src/index.js
+++ src/index.js
@@ -152,9 +152,7 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
 
             users.push(user)
@@ -168,10 +166,7 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             }
 
             posts.push(post)
@@ -187,9 +182,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post,
+                ...args
             }
 
             comments.push(comment)
