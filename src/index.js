11538066a2c61b05306b4eb9954ea767103d5be1
Wanchalerm S <tickstudiu@gmail.com>
2020-09-27 17:48:16 +0700

    update input type

--- src/index.js
+++ src/index.js
@@ -72,9 +72,28 @@ const typeDefs = `
     }
     
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createCommemt(text: String!, author: ID!, post: ID!): Comment!
+        createUser(data: CreateUserInput!): User!
+        createPost(data: CreatePostInput!): Post!
+        createCommemt(data: CreateCommentInput!): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!
+        author: ID!
+        post: ID!
     }
 
     type User {
@@ -147,12 +166,12 @@ const resolvers = {
     },
     Mutation: {
         createUser(parent, args, ctx, info){
-            const emailTaken = users.some((user) => user.email === args.email)
+            const emailTaken = users.some((user) => user.email === args.data.email)
             if(emailTaken) throw new Error("Email taken.")
 
             const user = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             users.push(user)
@@ -161,12 +180,12 @@ const resolvers = {
         },
 
         createPost(parent, args, ctx, info) {
-            const userExists = users.some(user => user.id === args.author)
+            const userExists = users.some(user => user.id === args.data.author)
             if(!userExists) throw new Error("Email taken.")
 
             const post = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             posts.push(post)
@@ -175,14 +194,14 @@ const resolvers = {
         },
 
         createCommemt(parent, args, ctx, info){
-            const userExists = users.some(user => user.id === args.author)
-            const postExists = posts.some(post => post.id === args.post && post.author)
+            const userExists = users.some(user => user.id === args.data.author)
+            const postExists = posts.some(post => post.id === args.data.post && post.published)
 
             if(!userExists || !postExists) throw new Error("Uable to find user and post.")
 
             const comment = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             comments.push(comment)
